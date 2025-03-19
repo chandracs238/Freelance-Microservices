4f1f644361206c845a2304e6aa3e91e3966b76e0
chandracs238 <chandhu7013@gmail.com>
2025-03-19 18:17:38 +0530

    updated Errors

---
+++ project-service/src/main/java/com/pcs/project_service/ProjectServiceApplication.java
@@ -0,0 +1,17 @@
+package com.pcs.project_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+public class ProjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectServiceApplication.class, args);
+	}
+
+}
